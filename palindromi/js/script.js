de67e2d36deeb4a04c64330d0854a719ed824422
MassimoAzzini <massimo.azzini.web@gmail.com>
2023-09-15 16:24:37 +0200

    [edit]: palindromi

--- palindromi/js/script.js
+++ palindromi/js/script.js
@@ -1,34 +1,33 @@
 // PALINDROMI
 
 const parolaUtente = prompt('Scrivi una parola').toLowerCase();
-console.log(parolaUtente)
+
+const outputMassaggio = document.getElementById('output');
 
 let parolaGirata = parolaContrario(parolaUtente);
+
 
 function parolaContrario (parola){
 
   const splitParola = parola.split('');
-  console.log(splitParola)
 
   const reversArray = splitParola.reverse();
-  console.log(reversArray)
 
   const joinArray = reversArray.join('');
   
-  console.log(joinArray);
-
   return joinArray;
 
 };
 
 
-if (parolaUtente == parolaGirata){
 
-  console.log('parola palindroma')
+if (parolaUtente === parolaGirata){
+
+  outputMassaggio.innerHTML = `${parolaUtente.toUpperCase()} è una parola palindroma`;
 
 }else{
 
-  console.log('parola NON palindroma')
+  outputMassaggio.innerHTML = `${parolaUtente.toUpperCase()} NON è una parola palindroma`;
 
 }
 
